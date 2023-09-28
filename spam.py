31bfa353fdd154d513649100fc0b6ad61024c99f
Ashtronut <146353030+Ashtronut@users.noreply.github.com>
2023-09-28 20:38:07 +0700

    Add files via upload

---
+++ spam.py
@@ -0,0 +1,17 @@
+import pyautogui as spam
+import time
+
+msg = input("Enter Message: ")
+
+count = 0
+limit = int(input("Jumlah: "))
+
+time.sleep(2)
+
+# while i<int(limit):
+    while count<int(limit)
+    count+=1
+    if count > limit:
+        break
+        spam.typwriter(msg)
+        spam.press('Enter')
